8701f1a1115de3fb191a5c24f408bf5d70907e6d
johnfracassi <johnffracassi@gmail.com>
2018-05-19 11:49:57 -0400

    add bootstrap data

--- build/generated/source/apt/main/com/rfb/domain/RfbUser_.java
+++ build/generated/source/apt/main/com/rfb/domain/RfbUser_.java
@@ -1,6 +1,7 @@
 package com.rfb.domain;
 
 import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
@@ -9,6 +10,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 public abstract class RfbUser_ {
 
 	public static volatile SingularAttribute<RfbUser, RfbLocation> homeLocation;
+	public static volatile SetAttribute<RfbUser, RfbEventAttendance> rfbEventAttendances;
 	public static volatile SingularAttribute<RfbUser, Long> id;
 	public static volatile SingularAttribute<RfbUser, String> username;
 
