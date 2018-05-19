572eb054c77a575a9c6a36da3c4e83d4bd063d18
johnfracassi <johnffracassi@gmail.com>
2018-05-19 08:04:40 -0400

    Generated Entities

---
+++ build/generated/source/apt/main/com/rfb/domain/RfbEventAttendance_.java
@@ -0,0 +1,18 @@
+package com.rfb.domain;
+
+import java.time.LocalDate;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(RfbEventAttendance.class)
+public abstract class RfbEventAttendance_ {
+
+	public static volatile SingularAttribute<RfbEventAttendance, Long> id;
+	public static volatile SingularAttribute<RfbEventAttendance, RfbUser> rfbUser;
+	public static volatile SingularAttribute<RfbEventAttendance, RfbEvent> rfbEvent;
+	public static volatile SingularAttribute<RfbEventAttendance, LocalDate> attendanceDate;
+
+}
+
---
+++ build/generated/source/apt/main/com/rfb/service/mapper/RfbEventAttendanceMapperImpl.java
@@ -0,0 +1,121 @@
+package com.rfb.service.mapper;
+
+import com.rfb.domain.RfbEvent;
+import com.rfb.domain.RfbEventAttendance;
+import com.rfb.domain.RfbUser;
+import com.rfb.service.dto.RfbEventAttendanceDTO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2018-05-19T07:54:28-0400",
+    comments = "version: 1.2.0.Final, compiler: javac, environment: Java 1.8.0_172 (Azul Systems, Inc.)"
+)
+@Component
+public class RfbEventAttendanceMapperImpl implements RfbEventAttendanceMapper {
+
+    @Autowired
+    private RfbEventMapper rfbEventMapper;
+    @Autowired
+    private RfbUserMapper rfbUserMapper;
+
+    @Override
+    public List<RfbEventAttendance> toEntity(List<RfbEventAttendanceDTO> dtoList) {
+        if ( dtoList == null ) {
+            return null;
+        }
+
+        List<RfbEventAttendance> list = new ArrayList<RfbEventAttendance>( dtoList.size() );
+        for ( RfbEventAttendanceDTO rfbEventAttendanceDTO : dtoList ) {
+            list.add( toEntity( rfbEventAttendanceDTO ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<RfbEventAttendanceDTO> toDto(List<RfbEventAttendance> entityList) {
+        if ( entityList == null ) {
+            return null;
+        }
+
+        List<RfbEventAttendanceDTO> list = new ArrayList<RfbEventAttendanceDTO>( entityList.size() );
+        for ( RfbEventAttendance rfbEventAttendance : entityList ) {
+            list.add( toDto( rfbEventAttendance ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public RfbEventAttendanceDTO toDto(RfbEventAttendance rfbEventAttendance) {
+        if ( rfbEventAttendance == null ) {
+            return null;
+        }
+
+        RfbEventAttendanceDTO rfbEventAttendanceDTO = new RfbEventAttendanceDTO();
+
+        Long id = rfbEventAttendanceRfbUserId( rfbEventAttendance );
+        if ( id != null ) {
+            rfbEventAttendanceDTO.setRfbUserId( id );
+        }
+        Long id1 = rfbEventAttendanceRfbEventId( rfbEventAttendance );
+        if ( id1 != null ) {
+            rfbEventAttendanceDTO.setRfbEventId( id1 );
+        }
+        rfbEventAttendanceDTO.setId( rfbEventAttendance.getId() );
+        rfbEventAttendanceDTO.setAttendanceDate( rfbEventAttendance.getAttendanceDate() );
+
+        return rfbEventAttendanceDTO;
+    }
+
+    @Override
+    public RfbEventAttendance toEntity(RfbEventAttendanceDTO rfbEventAttendanceDTO) {
+        if ( rfbEventAttendanceDTO == null ) {
+            return null;
+        }
+
+        RfbEventAttendance rfbEventAttendance = new RfbEventAttendance();
+
+        rfbEventAttendance.setRfbEvent( rfbEventMapper.fromId( rfbEventAttendanceDTO.getRfbEventId() ) );
+        rfbEventAttendance.setRfbUser( rfbUserMapper.fromId( rfbEventAttendanceDTO.getRfbUserId() ) );
+        rfbEventAttendance.setId( rfbEventAttendanceDTO.getId() );
+        rfbEventAttendance.setAttendanceDate( rfbEventAttendanceDTO.getAttendanceDate() );
+
+        return rfbEventAttendance;
+    }
+
+    private Long rfbEventAttendanceRfbUserId(RfbEventAttendance rfbEventAttendance) {
+        if ( rfbEventAttendance == null ) {
+            return null;
+        }
+        RfbUser rfbUser = rfbEventAttendance.getRfbUser();
+        if ( rfbUser == null ) {
+            return null;
+        }
+        Long id = rfbUser.getId();
+        if ( id == null ) {
+            return null;
+        }
+        return id;
+    }
+
+    private Long rfbEventAttendanceRfbEventId(RfbEventAttendance rfbEventAttendance) {
+        if ( rfbEventAttendance == null ) {
+            return null;
+        }
+        RfbEvent rfbEvent = rfbEventAttendance.getRfbEvent();
+        if ( rfbEvent == null ) {
+            return null;
+        }
+        Long id = rfbEvent.getId();
+        if ( id == null ) {
+            return null;
+        }
+        return id;
+    }
+}
