beca197563543cf24964eb3e2cb82c7f10eb4072
johnfracassi <johnffracassi@gmail.com>
2018-05-22 20:45:36 -0400

    added facebook and twitter integration

--- src/main/java/com/rfb/service/SocialService.java
+++ src/main/java/com/rfb/service/SocialService.java
@@ -5,7 +5,6 @@ import com.rfb.domain.User;
 import com.rfb.repository.AuthorityRepository;
 import com.rfb.repository.UserRepository;
 import com.rfb.security.AuthoritiesConstants;
-
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -17,6 +16,9 @@ import org.springframework.social.connect.UserProfile;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Optional;
@@ -36,6 +38,28 @@ public class SocialService {
     private final UserRepository userRepository;
 
     private final MailService mailService;
+
+    // This postconstruct method is only required because of a bug in Spring Social and can be removed once fixed.
+    @PostConstruct
+    private void init() {
+        try {
+            String[] fieldsToMap = {
+                "id", "about", "age_range", "birthday", "context", "cover", "currency", "devices", "education", "email", "favorite_athletes", "favorite_teams", "first_name", "gender", "hometown", "inspirational_people", "installed", "install_type","is_verified", "languages", "last_name", "link", "locale", "location", "meeting_for", "middle_name", "name", "name_format","political", "quotes", "payment_pricepoints", "relationship_status", "religion", "security_settings", "significant_other","sports", "test_group", "timezone", "third_party_id", "updated_time", "verified", "viewer_can_send_gift","website", "work"
+            };
+
+            Field field = Class.forName("org.springframework.social.facebook.api.UserOperations").
+                getDeclaredField("PROFILE_FIELDS");
+            field.setAccessible(true);
+
+            Field modifiers = field.getClass().getDeclaredField("modifiers");
+            modifiers.setAccessible(true);
+            modifiers.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+            field.set(null, fieldsToMap);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public SocialService(UsersConnectionRepository usersConnectionRepository, AuthorityRepository authorityRepository,
             PasswordEncoder passwordEncoder, UserRepository userRepository,
