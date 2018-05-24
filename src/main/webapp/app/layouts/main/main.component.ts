f465cc973c58d08d433fe43c7d5ef801c74c80a4
johnfracassi <johnffracassi@gmail.com>
2018-05-24 07:11:57 -0400

    now setting and using public current route

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -1,26 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRouteSnapshot, NavigationEnd, Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
 
-import {Title} from '@angular/platform-browser';
+import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'jhi-main',
     templateUrl: './main.component.html'
 })
 export class JhiMainComponent implements OnInit {
+    public currentRoute: string;
 
-    cardStyles: string;
-
-    constructor(
-        private titleService: Title,
-        private router: Router
-    ) {}
+    constructor(private titleService: Title, private router: Router) {}
 
     private getPageTitle(routeSnapshot: ActivatedRouteSnapshot) {
         let title: string = (routeSnapshot.data && routeSnapshot.data['pageTitle']) ? routeSnapshot.data['pageTitle'] : 'rfbloyaltyApp';
         if (routeSnapshot.firstChild) {
             title = this.getPageTitle(routeSnapshot.firstChild) || title;
         }
+        this.currentRoute = this.router.url;
         return title;
     }
 
